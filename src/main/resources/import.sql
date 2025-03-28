9c235b46d4ff3b4f4a0b27f6713def6f597f1b1b
Jherna77 <85742055+Jherna77@users.noreply.github.com>
2025-03-28 22:17:40 +0100

    Se completa la tabla users_roles

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -14,7 +14,10 @@ INSERT INTO users (id, email, password, name, surnames, address, city, province,
 
 -- Asignación de roles
 INSERT INTO users_roles (user_id, role_id) VALUES (1, 1);
+INSERT INTO users_roles (user_id, role_id) VALUES (1, 2);
+INSERT INTO users_roles (user_id, role_id) VALUES (1, 3);
 INSERT INTO users_roles (user_id, role_id) VALUES (2, 2);
+INSERT INTO users_roles (user_id, role_id) VALUES (2, 3);
 INSERT INTO users_roles (user_id, role_id) VALUES (3, 3);
 
 -- Categorías de productos
