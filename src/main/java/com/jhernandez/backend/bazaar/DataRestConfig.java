fd7df9e73839f26a4e7a2ecdf8f3ed9370b7b216
DiTToLapTop <dittodell@gmail.com>
2025-03-28 17:49:32 +0100

    Eliminacion de warnings

--- src/main/java/com/jhernandez/backend/bazaar/DataRestConfig.java
+++ src/main/java/com/jhernandez/backend/bazaar/DataRestConfig.java
@@ -1,11 +1,11 @@
 package com.jhernandez.backend.bazaar;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+// import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
-import com.jhernandez.backend.bazaar.entities.UserEntity;
+// import com.jhernandez.backend.bazaar.entities.UserEntity;
 
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer{
--- src/main/java/com/jhernandez/backend/bazaar/repositories/UserRepository.java
+++ src/main/java/com/jhernandez/backend/bazaar/repositories/UserRepository.java
@@ -1,7 +1,7 @@
 package com.jhernandez.backend.bazaar.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+// import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.jhernandez.backend.bazaar.entities.UserEntity;
 
