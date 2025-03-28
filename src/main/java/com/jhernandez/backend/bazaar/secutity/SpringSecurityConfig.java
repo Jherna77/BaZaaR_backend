842d826592da315a09ef73a552fd5c942954f4ec
DiTToLapTop <dittodell@gmail.com>
2025-03-28 17:50:54 +0100

    Spring Security

---
+++ src/main/java/com/jhernandez/backend/bazaar/secutity/SpringSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.jhernandez.backend.bazaar.secutity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+            .requestMatchers("/api/users").permitAll() // Se dejan públicos los endpoints relacionados con users
+            .anyRequest().authenticated()) // Los demás requieren autenticación
+            .csrf(config -> config.disable()) // Deshabilita CSRF para evitar vulnerabilidades (p.e. en formularios)
+            .sessionManagement(management ->
+                management.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Sesión sin estado para manejar en el token todo lo relacionado con autenticación
+            .build();
+    }
+
+}
