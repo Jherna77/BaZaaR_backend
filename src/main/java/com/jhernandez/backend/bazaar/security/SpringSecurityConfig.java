045187bd85790fe04553f1ea907cf70341b5f41d
Jherna77 <85742055+Jherna77@users.noreply.github.com>
2025-03-28 22:24:14 +0100

    Corrección del nombre del paquete

--- src/main/java/com/jhernandez/backend/bazaar/security/SpringSecurityConfig.java
+++ src/main/java/com/jhernandez/backend/bazaar/security/SpringSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.jhernandez.backend.bazaar.secutity;
+package com.jhernandez.backend.bazaar.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
