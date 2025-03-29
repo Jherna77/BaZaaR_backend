dc9afdcb2a0c61073d2fd45678a7e0a2b4537ea6
Jherna77 <85742055+Jherna77@users.noreply.github.com>
2025-03-29 09:35:35 +0100

    permisos de Spring Security

--- src/main/java/com/jhernandez/backend/bazaar/security/SpringSecurityConfig.java
+++ src/main/java/com/jhernandez/backend/bazaar/security/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package com.jhernandez.backend.bazaar.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,7 +20,8 @@ public class SpringSecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests
-            .requestMatchers("/api/users").permitAll() // Se dejan públicos los endpoints relacionados con users
+            .requestMatchers(HttpMethod.GET, "/api/users").permitAll() // Se permiten las peticiones GET a /api/users
+            .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll() // Se permiten las peticiones POST a /api/users/register
             .anyRequest().authenticated()) // Las demás peticiones requieren autenticación
             .csrf(config -> config.disable()) // Deshabilita CSRF para evitar vulnerabilidades (p.e. en formularios)
             .sessionManagement(management ->
