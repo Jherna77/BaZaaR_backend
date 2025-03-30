e7306451f31622147202a9b6c21aef8cc80ba0a9
Jherna77 <85742055+Jherna77@users.noreply.github.com>
2025-03-30 18:45:17 +0200

    Comentarios

--- src/main/java/com/jhernandez/backend/bazaar/validation/RequiredFieldValidator.java
+++ src/main/java/com/jhernandez/backend/bazaar/validation/RequiredFieldValidator.java
@@ -5,6 +5,10 @@ import org.springframework.util.StringUtils;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+/*
+ * Valida que un campo no esté vacío o nulo.
+ * Se utiliza en la validación de campos obligatorios en formularios.
+ */
 public class RequiredFieldValidator implements ConstraintValidator<RequiredField, String> {
 
     @Override
--- src/main/java/com/jhernandez/backend/bazaar/validation/ZipCodeValidator.java
+++ src/main/java/com/jhernandez/backend/bazaar/validation/ZipCodeValidator.java
@@ -1,19 +1,18 @@
 package com.jhernandez.backend.bazaar.validation;
 
-import java.util.regex.Pattern;
-
 import jakarta.validation.ConstraintValidator;
 
-// Se comprueba que el código postal tenga 5 dígitos
+/*
+ * Valida que un código postal tenga 5 dígitos.
+ * Se utiliza en la validación de códigos postales en formularios.
+ */
 public class ZipCodeValidator implements ConstraintValidator<ZipCode, String> {
 
-    // private static final Pattern ZIP_CODE_PATTERN = Pattern.compile("^[0-9]{5}$");
     private static final String ZIP_CODE_PATTERN = "^[0-9]{5}$";
 
     @Override
     public boolean isValid(String zipCode, jakarta.validation.ConstraintValidatorContext context) {
         
         return zipCode.matches(ZIP_CODE_PATTERN);
-        // return ZIP_CODE_PATTERN.matcher(zipCode).matches();    
     }
 }
