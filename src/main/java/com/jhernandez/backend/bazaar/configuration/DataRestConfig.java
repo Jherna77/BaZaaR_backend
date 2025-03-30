3d08468a55627f8c23541f79e1ba166dbdd5021e
Jherna77 <85742055+Jherna77@users.noreply.github.com>
2025-03-30 11:10:25 +0200

    Creacion de paquete validation

--- src/main/java/com/jhernandez/backend/bazaar/configuration/DataRestConfig.java
+++ src/main/java/com/jhernandez/backend/bazaar/configuration/DataRestConfig.java
@@ -1,4 +1,4 @@
-package com.jhernandez.backend.bazaar;
+package com.jhernandez.backend.bazaar.configuration;
 
 import org.springframework.context.annotation.Configuration;
 // import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
