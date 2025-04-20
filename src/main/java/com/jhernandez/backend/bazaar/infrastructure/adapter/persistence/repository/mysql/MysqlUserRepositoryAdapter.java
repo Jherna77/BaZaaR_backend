1be0e9f0af34d9453886d05b47d986c7b9b90e49
DiTToDualCore <dittodell@gmail.com>
2025-04-20 21:45:27 +0200

    Necesito vacaciones

--- src/main/java/com/jhernandez/backend/bazaar/infrastructure/adapter/persistence/repository/mysql/MysqlUserRepositoryAdapter.java
+++ src/main/java/com/jhernandez/backend/bazaar/infrastructure/adapter/persistence/repository/mysql/MysqlUserRepositoryAdapter.java
@@ -90,7 +90,7 @@ public class MysqlUserRepositoryAdapter implements UserRepositoryPort {
     public Optional<User> enableUserById(Long id) {
         log.info("Enabling user with ID {}", id);
         return userRepository.findById(id).map(user -> {
-            user.setName(user.getName().replace(DISABLED_ITEM, ""));
+            // user.setName(user.getName().replace(DISABLED_ITEM, ""));
             user.setEnabled(true);
             return userEntityMapper.toDomain(userRepository.save(user));
         });
@@ -100,7 +100,7 @@ public class MysqlUserRepositoryAdapter implements UserRepositoryPort {
     public Optional<User> disableUserById(Long id) {
         log.info("Disabling user with ID {}", id);
         return userRepository.findById(id).map(user -> {
-            user.setName(user.getName() + DISABLED_ITEM);
+            // user.setName(user.getName() + DISABLED_ITEM);
             user.setEnabled(false);
             return userEntityMapper.toDomain(userRepository.save(user));
         });
